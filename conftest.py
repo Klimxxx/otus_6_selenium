5fd1d86de238bf74e7fdf129c6ac2af687b558e7
Klim Trotsenko <klim.trotsenko7@gmail.com>
2024-06-22 08:28:18 +0300

    update conftest.py add webdrivermanager. checked tests on local machine

--- conftest.py
+++ conftest.py
@@ -1,18 +1,25 @@
+import pytest
+import os
+import time
+
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FFOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
 
-import pytest
-
+# options.add_argument('==window_size=1920, 1080')
+# service = Service(executable_path=ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=service, options=options)
 
 def pytest_addoption(parser):
     parser.addoption(
         "--browser", default="chrome", help="choose the browser that you want to use"
     )
     parser.addoption("--headless", action="store_true")
-    parser.addoption("--base_url", default="http://localhost")
-
+    parser.addoption("--base_url", action="store", default=f"http://localhost/", help="Base URL for testing")
 
 @pytest.fixture()
 def browser(request):
@@ -25,21 +32,22 @@ def browser(request):
         options = ChromeOptions()
         if headless:
             options.add_argument("--headless=new")
-        service = ChromeService(
-            executable_path="/Users/klim/Downloads/chromedriver-mac-arm64/chromedriver"
-        )
+        service = Service(executable_path=ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
     elif browser_name == "firefox":
         options = FFOptions()
         if headless:
             options.add_argument("-headless")
+        service = Service(executable_path=GeckoDriverManager().install())
         driver = webdriver.Firefox(options=options)
+    else:
+        raise Exception("Driver not supported")
 
     driver.maximize_window()
 
     driver.base_url = base_url
 
-    yield driver
+    request.addfinalizer(driver.quit)
 
-    if driver is not None:
-        driver.quit()
+
+    return driver
